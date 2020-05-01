a02050cf5ac97e7af6409e50d46e02f0abe2caf0
Roy <Roywhite@sjtu.edu.cn>
2020-05-01 15:14:02 +0800

    update meme prediect motif

--- 提序列.py
+++
@@ -1,6 +0,0 @@
-with open("预测ORF+blast结果定位NLP序列.txt")as file:
-	lines = file.readlines()
-	for line in lines:
-		if line.startswith('>'):
-			with open('out','a') as fw:
-				fw.write(line + lines[lines.index(line)+1])
--- 超级序列简化器.py
+++
@@ -1,7 +0,0 @@
-import re
-string = '>WP_048261218.1 sugar-binding protein [Pectobacterium peruviense]'
-
-pattern = re.compile(r'^(\>[\w\.]+)\s([\w\-\s\W]+?)\[([A-Za-z]+[\s]*[A-Za-z]+).*\]$')
-a = pattern.match(string)
-print(a.group(1))
-print(a.group(3))
